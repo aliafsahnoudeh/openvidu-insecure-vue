5a36ad35774d43647ec3c41d243134086b2e3eeb
Ali <aliafsah1988@gmail.com>
2020-01-29 06:33:39 +0330

    init

---
+++ src/components/OvVideo.vue
@@ -0,0 +1,34 @@
+<template>
+  <video autoPlay="true" ref="videoRef" :id="streamManager.stream.connection.connectionId"/>
+</template>
+
+<script>
+export default {
+     props:{
+        streamManager: {
+            type: Object,
+            default() {
+                return null;
+            }
+        }
+    },
+    methods:{
+        addVideoElement() {
+        if (this.streamManager) {
+            const element = document.getElementById(this.streamManager.stream.connection.connectionId)
+            this.streamManager.addVideoElement(element);
+        }
+        }
+    },
+    mounted() {
+        this.addVideoElement()
+    },
+    updated() {
+        this.addVideoElement()
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/UserVideoComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="streamcomponent">
+        <OpenViduVideoComponent :streamManager="streamManager" />
+        <div><p>{{ getNicknameTag() }}</p></div>
+    </div>
+</template>
+
+<script>
+import OpenViduVideoComponent from './OvVideo';
+
+export default {
+    components: {
+        OpenViduVideoComponent
+    },
+    props:{
+        streamManager: {
+            type: Object,
+            default() {
+                return null;
+            }
+        }
+    },
+    methods: {
+        getNicknameTag() {
+            // Gets the nickName of the user
+            return JSON.parse(this.streamManager.stream.connection.data).clientData;
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+video {
+    width: 100%;
+    height: auto;
+    float: left;
+    cursor: pointer;
+}
+.streamcomponent div {
+    position: absolute;
+    background: #f8f8f8;
+    padding-left: 5px;
+    padding-right: 5px;
+    color: #777777;
+    font-weight: bold;
+    border-bottom-right-radius: 4px;
+}
+p{
+    margin: 0;
+}
+</style>
