cca006854f9d0bb2d4ca356a3b789e6edc3bc8d2
Ali Afsahnoudeh <ali.afsah1988@gmail.com>
2020-07-20 21:53:45 +0430

    api service

--- src/components/Join/index.vue
+++ src/components/Join/index.vue
@@ -38,9 +38,10 @@
 
 <script>
 import { OpenVidu } from 'openvidu-browser'
-import axios from 'axios'
-import config from '../../config'
 import { mapActions, mapState } from 'vuex'
+import ApiService from '@/services/api.service'
+
+const apiService = new ApiService()
 
 export default {
   name: 'Join',
@@ -89,7 +90,7 @@ export default {
 
       // 'getToken' method is simulating what your server-side should do.
       // 'token' parameter should be retrieved and returned by your own backend
-      this.getToken().then((token) => {
+      apiService.getToken(this.sessionId).then((token) => {
         // First param is the token got from OpenVidu Server. Second param can be retrieved by every user on event
         // 'streamCreated' (property Stream.connection.data), and will be appended to DOM as the user's nickname
         this.session
@@ -131,81 +132,6 @@ export default {
     },
     handleChangeUserName(e) {
       this.setUsername(e.target.value)
-    },
-    /**
-     * --------------------------
-     * SERVER-SIDE RESPONSIBILITY
-     * --------------------------
-     * These methods retrieve the mandatory user token from OpenVidu Server.
-     * This behavior MUST BE IN YOUR SERVER-SIDE IN PRODUCTION (by using
-     * the API REST, openvidu-java-client or openvidu-node-client):
-     *   1) Initialize a session in OpenVidu Server	(POST /api/sessions)
-     *   2) Generate a token in OpenVidu Server		(POST /api/tokens)
-     *   3) The token must be consumed in Session.connect() method
-     */
-    getToken() {
-      return this.createSession().then((id) => this.createToken(id))
-    },
-    createSession() {
-      return new Promise((resolve, reject) => {
-        var data = JSON.stringify({ customSessionId: this.sessionId })
-        console.log(`createSession: ${data}`)
-
-        axios
-          .post(config.OPENVIDU_SERVER_URL + '/api/sessions', data, {
-            headers: {
-              Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + config.OPENVIDU_SERVER_SECRET),
-              'Content-Type': 'application/json'
-            }
-          })
-          .then((response) => {
-            console.log('CREATE SESION', response)
-            resolve(response.data.id)
-          })
-          .catch((response) => {
-            console.error(response)
-            var error = Object.assign({}, response)
-            if (error.response.status === 409) {
-              resolve(this.sessionId)
-            } else {
-              console.log(error)
-              console.warn(
-                'No connection to OpenVidu Server. This may be a certificate error at ' +
-                            config.OPENVIDU_SERVER_URL
-              )
-              if (
-                window.confirm(
-                  'No connection to OpenVidu Server. This may be a certificate error at "' +
-                                config.OPENVIDU_SERVER_URL +
-                                '"\n\nClick OK to navigate and accept it. ' +
-                                'If no certificate warning is shown, then check that your OpenVidu Server is up and running at "' +
-                                config.OPENVIDU_SERVER_URL +
-                                '"'
-                )
-              ) {
-                window.location.assign(config.OPENVIDU_SERVER_URL + '/accept-certificate')
-              }
-            }
-          })
-      })
-    },
-    createToken(sessionId) {
-      return new Promise((resolve, reject) => {
-        console.log(`createToken: ${sessionId}`)
-        var data = JSON.stringify({ session: sessionId })
-        axios
-          .post(config.OPENVIDU_SERVER_URL + '/api/tokens', data, {
-            headers: {
-              Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + config.OPENVIDU_SERVER_SECRET),
-              'Content-Type': 'application/json'
-            }
-          })
-          .then((response) => {
-            console.log('TOKEN', response)
-            resolve(response.data.token)
-          })
-          .catch((error) => reject(error))
-      })
     }
   }
 }
